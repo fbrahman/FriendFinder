a9b36ad8a03bd78f8631763883cc86b4e8f1a15e
fbrahman <fbrahman@gmail.com>
2017-07-16 15:19:37 -0700

    post request working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 let PORT = process.env.PORT || 3000;
@@ -17,14 +18,33 @@ app.use(bodyParser.json({
 app.use(express.static(path.join(__dirname, 'app/public')))
 
 
-app.get('/apiFriendList', function (req,res){
-	res.sendfile(path.join(__dirname,'friends.json'));
+app.get('/apiFriendList', (req, res) => {
+    res.sendfile(path.join(__dirname, 'friends.json'));
 })
 
-app.get('/survey', function (req, res){
-	res.sendfile(path.join(__dirname, 'app/public/survey.html'))
+app.get('/survey', (req, res) => {
+    res.sendfile(path.join(__dirname, 'app/public/survey.html'))
 })
 
-app.listen(PORT, function(){
-	console.log('App listening on PORT' + PORT);
+app.post('/newperson', (req, res) => {
+    let newPerson = req.body;
+    // let friendsArray = [];
+    res.status(200).send(true)
+
+    fs.readFile(path.join(__dirname, 'friends.json'), (err, data) => {
+        if (err) throw err;
+
+        let friendArray = JSON.parse(data);
+
+        friendArray.push(newPerson);
+
+        fs.writeFile(path.join(__dirname, 'friends.json'), JSON.stringify(friendArray, null, 2), 'utf-8', (err) => {
+            if (err) throw err;
+            console.log("file updated!");
+        })
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('App listening on PORT' + PORT);
 })
