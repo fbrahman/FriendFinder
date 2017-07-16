4eff125b1714552b091e99d2011323c8d202b1c3
fbrahman <fbrahman@gmail.com>
2017-07-15 19:03:37 -0700

    Survey page formatting

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path')
 
 const app = express();
-let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
