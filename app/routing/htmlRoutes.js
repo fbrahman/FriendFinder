140ba4816339eaae87650b013ae791dda0ecf1e1
fbrahman <fbrahman@gmail.com>
2017-07-21 21:07:37 -0700

    done before heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 
 let router = express.Router();
 
 router.use(express.static(path.join(__dirname, '../public')))
+
+router.get('/', (req,res)=>{
+	res.sendFile(path.join(__dirname, '../public/home.html'))
+})
 
 router.get('/survey', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/survey.html'))
